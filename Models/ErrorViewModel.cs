481a1ba2713bb0400ba0865fd256bf28c11b487d
uChusta <118682761+uChusta@users.noreply.github.com>
2026-08-23 09:38:51 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lecturer_Cliam_Register.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
